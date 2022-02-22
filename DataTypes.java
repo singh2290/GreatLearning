c7a68af29bf975e945284429544cb8637eb0902f
prateek <singhprateek702@gmail.com>
2022-02-22 23:02:53 +0530

    DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+class DataTypes{
+	public static void main(String[] args) {
+
+	int a = 10;
+	float b= 20;
+	boolean c=true;
+	String d= "prateek";
+	double e= 156.99;
+	char f= 'D';
+	double g= 90.99;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(g);
+System.out.println(f);
+
+	}
+}  
